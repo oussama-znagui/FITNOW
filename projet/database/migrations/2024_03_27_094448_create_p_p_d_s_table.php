21650705b910fbb777f99833f7262afef31b467c
oussama <znagui.info@gmail.com>
2024-03-27 10:15:15 +0000

    creation de table des progression sportive

--- projet/database/migrations/2024_03_27_094448_create_p_p_d_s_table.php
+++ projet/database/migrations/2024_03_27_094448_create_p_p_d_s_table.php
@@ -13,7 +13,12 @@ return new class extends Migration
     {
         Schema::create('p_p_d_s', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->string('title');
+            $table->float('weight');
+            $table->float('size');
+            $table->foreignId('user_id')->constrained('users');
+            $table->boolean('status')->default(0);
+            $table->$table->timestamps();
         });
     }
 
